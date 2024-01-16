291e4c0196035c13a0e5ff547c25328e39c8bf79
ElegantLee <superlee.handso@gmail.com>
2024-01-16 12:16:29 +0800

    style: 安装 emotion,优化登录注册界面

--- src/screens/unauthenticated-app/login.tsx
+++ src/screens/unauthenticated-app/login.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button, Form, Input } from 'antd';
 import { useAuth } from '../context/auth-context';
+import { LongButton } from '.';
 
 export const LoginScreen = () => {
   const { login, user } = useAuth();
@@ -17,23 +18,21 @@ export const LoginScreen = () => {
         </div>
       ) : null}
       <Form.Item
-        label={'用户名'}
         name={'username'}
         rules={[{ required: true, message: '请输入用户名' }]}
       >
         <Input placeholder={'用户名'} type="text" />
       </Form.Item>
       <Form.Item
-        label={'密码'}
         name={'password'}
         rules={[{ required: true, message: '请输入密码' }]}
       >
         <Input placeholder={'密码'} type={'password'} />
       </Form.Item>
       <Form.Item>
-        <Button type={'primary'} htmlType={'submit'}>
+        <LongButton type={'primary'} htmlType={'submit'}>
           登录
-        </Button>
+        </LongButton>
       </Form.Item>
     </Form>
   );
--- src/screens/unauthenticated-app/register.tsx
+++ src/screens/unauthenticated-app/register.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useAuth } from '../context/auth-context';
 import { Button, Form, Input } from 'antd';
+import { LongButton } from '.';
 
 export const RegisterScreen = () => {
   const { register } = useAuth();
@@ -11,23 +12,21 @@ export const RegisterScreen = () => {
   return (
     <Form onFinish={handleSubmit}>
       <Form.Item
-        label={'用户名'}
         name={'username'}
         rules={[{ required: true, message: '请输入用户名' }]}
       >
         <Input placeholder={'用户名'} type="text" />
       </Form.Item>
       <Form.Item
-        label={'密码'}
         name={'password'}
         rules={[{ required: true, message: '请输入密码' }]}
       >
         <Input placeholder={'密码'} type={'password'} />
       </Form.Item>
       <Form.Item>
-        <Button type={'primary'} htmlType={'submit'}>
+        <LongButton type={'primary'} htmlType={'submit'}>
           注册
-        </Button>
+        </LongButton>
       </Form.Item>
     </Form>
   );
