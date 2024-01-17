768399dc4d0410db1d444e729be8a7ed946969df
ElegantLee <superlee.handso@gmail.com>
2024-01-17 17:27:53 +0800

    feat: 登录和注册 loading 和错误处理

--- src/screens/unauthenticated-app/index.tsx
+++ src/screens/unauthenticated-app/index.tsx
@@ -1,21 +1,33 @@
 import React, { useState } from 'react';
 import { RegisterScreen } from './register';
 import { LoginScreen } from './login';
-import { Button, Card, Divider } from 'antd';
+import { Button, Card, Divider, Typography } from 'antd';
 import styled from '@emotion/styled';
 import logo from 'assets/logo.svg';
 import left from 'assets/left.svg';
 import right from 'assets/right.svg';
 
+const { Text } = Typography;
+
 export const UnauthenticatedApp = () => {
   const [isRegister, setIsRegister] = useState(false);
+  const [error, setError] = useState<Error | null>(null);
   return (
     <Container>
       <Header />
       <Background />
       <ShadowCard>
         <Title>{isRegister ? '请注册' : '请登录'}</Title>
-        {isRegister ? <RegisterScreen /> : <LoginScreen />}
+        {error ? (
+          <Typography>
+            <Text type={'danger'}>{error.message}</Text>
+          </Typography>
+        ) : null}
+        {isRegister ? (
+          <RegisterScreen onError={setError} />
+        ) : (
+          <LoginScreen onError={setError} />
+        )}
         <Divider />
         <button onClick={() => setIsRegister(!isRegister)}>
           {isRegister ? '已有账号？直接登录' : '没有账号？注册新账号'}
--- src/screens/unauthenticated-app/register.tsx
+++ src/screens/unauthenticated-app/register.tsx
@@ -2,11 +2,32 @@ import React from 'react';
 import { useAuth } from '../context/auth-context';
 import { Form, Input } from 'antd';
 import { LongButton } from '.';
+import { useAsync } from 'utils/use-async';
 
-export const RegisterScreen = () => {
+export const RegisterScreen = ({
+  onError,
+}: {
+  onError: (error: Error) => void;
+}) => {
   const { register } = useAuth();
-  const handleSubmit = (values: { username: string; password: string }) => {
-    register(values);
+  const { run, isLoading } = useAsync(undefined, { throwOnError: true });
+  const handleSubmit = async ({
+    cpassword,
+    ...values
+  }: {
+    cpassword: string;
+    username: string;
+    password: string;
+  }) => {
+    if (cpassword !== values.password) {
+      onError(new Error('请确认两次输入的密码是否相同'));
+      return;
+    }
+    try {
+      await run(register(values));
+    } catch (e) {
+      onError(e as Error);
+    }
   };
 
   return (
@@ -23,8 +44,14 @@ export const RegisterScreen = () => {
       >
         <Input placeholder={'密码'} type={'password'} />
       </Form.Item>
+      <Form.Item
+        name={'cpassword'}
+        rules={[{ required: true, message: '请输入密码' }]}
+      >
+        <Input placeholder={'确认密码'} type={'password'} id={'cpassword'} />
+      </Form.Item>
       <Form.Item>
-        <LongButton type={'primary'} htmlType={'submit'}>
+        <LongButton loading={isLoading} type={'primary'} htmlType={'submit'}>
           注册
         </LongButton>
       </Form.Item>
